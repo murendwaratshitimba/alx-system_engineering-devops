b84319ecd0f3bc179a9843d57715e29305d07980
Murendwa Ratshitimba <murendwaratshitimba@gmail.com>
2023-04-26 03:35:08 +0200

    5. Install Nginx web server (w/ Puppet)

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# install and configure an Nginx server using Puppet
+package { 'nginx':
+  ensure => installed,
+}
+
+file_line { 'new_page':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/@murendwaratshitimba permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
