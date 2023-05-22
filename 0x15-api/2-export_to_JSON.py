1e19685c1172e0fb21e330daca5a4419d67bbcca
Murendwa Ratshitimba <murendwaratshitimba@gmail.com>
2023-05-22 21:57:20 +0200

    2. Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script to export data in the JSON format"""
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    employee = requests.get(url + "users/{}".format(user_id)).json()
+    username = employee.get("username")
+    tasks = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.json".format(user_id), "w") as jsonfile:
+        json.dump({user_id: [{
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": username
+            } for task in tasks]}, jsonfile)
