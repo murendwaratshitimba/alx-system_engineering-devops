74ab2a625032e68149bd0603d821d75f3a8f44cf
Murendwa Ratshitimba <murendwaratshitimba@gmail.com>
2023-10-11 00:37:24 +0200

    Task 2 complete

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Task 2: Recurse it!"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], count=0, after=None):
+    """Queries the Reddit API and returns all hot posts
+    of the subreddit"""
+
+    info = requests.get("https://www.reddit.com/r/{}/hot.json"
+                        .format(subreddit),
+                        params={"count": count, "after": after},
+                        headers={"User-Agent": "My-User-Agent"},
+                        allow_redirects=False)
+
+    info_json = info.json()
+
+    if info.status_code >= 400:
+        return None
+
+    hot_articles = hot_list + [child.get("data").get("title")
+                               for child in info_json
+                               .get("data")
+                               .get("children")]
+
+    if not info_json.get("data").get("after"):
+        return hot_articles
+
+    return recurse(subreddit, hot_articles, info_json.get("data").get("count"),
+                   info_json.get("data").get("after"))
