98caff1fc77b7b44ac72f24d51747ffbfa4bd027
Murendwa Ratshitimba <murendwaratshitimba@gmail.com>
2023-10-20 01:00:19 +0200

    1. User limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,15 @@
+# Task 1: User limit
+
+exec { 'increase-hard-limit':
+
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-limit':
+
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+
+  path    => '/usr/local/bin/:/bin/'
+}
